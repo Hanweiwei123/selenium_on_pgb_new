bab8920e419215c02dad414c6104f394a39585df
Hanweiwei123 <weiwei.han@dilatoit.com>
2013-10-18 13:36:56 +0800

    Update TC_015_app_id_builds_strings_spec.rb
    1.update server and account
    2.update download_with_different_browser
    3.update timeout and catch exception

--- testcase/TC_015_app_id_builds_strings_spec.rb
+++ testcase/TC_015_app_id_builds_strings_spec.rb
@@ -17,12 +17,13 @@ describe "TC_015: App Details #Builds" do
   include NewAppLocator
   include AppIdLocator
 
-  before(:all) do 
+  before(:all) do
     init
     @order_of_it = WebdriverHelper::Counter.new
     @name_screenshot = "TC_015_IT_"
-    #@base_url = base_url
-    @base_url = "https://build.phonegap.com"
+    @base_url = base_url
+    #@base_url = "https://buildstage.phonegap.com"
+    #@base_url = "https://build.phonegap.com"
     @driver = driver
     @driver.manage.timeouts.implicit_wait = 30
     @download_dir = Dir.home + "/Downloads"
@@ -30,242 +31,204 @@ describe "TC_015: App Details #Builds" do
     @driver.execute_script("window.resizeTo(screen.width,screen.height)")
 
     @new_app_page = NewAppPage.new :driver => @driver,
-                                   :base_url =>@base_url ,
-                                   :user => { :id => "shuai.yan@dilatoit.com", :password => "yanshuai110" }
-                                   # :user => {:id => $data[:user][$lang][:adobe_id_free_002][:id], :password =>  $data[:user][$lang][:adobe_id_free_002][:password] }
-    @new_app_page.new_app_with_zip;  sleep 15
+                                   :base_url => @base_url,
+                                   #:user => { :id => "shuai.yan@dilatoit.com", :password => "yanshuai110" }
+                                   :user => {:id => $data[:user][$lang][:adobe_id_free_002][:id], :password => $data[:user][$lang][:adobe_id_free_002][:password]}
+    @new_app_page.new_app_with_zip; sleep 15
     @app_id = @new_app_page.first_app_id
-    new_app_locator(:ready_to_build_btn).click;  sleep 10
+    new_app_locator(:ready_to_build_btn).click; sleep 10
     @driver.get "#{@base_url}\/apps\/#{@app_id}\/builds"
     @current_url = @driver.current_url
     puts "+<current_url> is #{@current_url}"; sleep 10
   end
 
-  after(:all) do 
-    begin 
-      # webhelper_delete_all_apps $data[:user][$lang][:adobe_id_free_002][:id], $data[:user][$lang][:adobe_id_free_002][:password]
-      #webhelper_delete_all_signing_keys $data[:user][$lang][:adobe_id_free_002][:id], $data[:user][$lang][:adobe_id_free_002][:password]
-      webhelper_delete_all_apps "shuai.yan@dilatoit.com", "yanshuai110"
-      webhelper_delete_all_signing_keys "shuai.yan@dilatoit.com", "yanshuai110"
-    ensure
-      @driver.quit
-    end
+  after(:all) do
+    webhelper_delete_all_apps $data[:user][$lang][:adobe_id_free_002][:id], $data[:user][$lang][:adobe_id_free_002][:password]
+    #webhelper_delete_all_apps "shuai.yan@dilatoit.com", "yanshuai110"
+    @driver.quit
   end
 
-  after(:each) do  # Take screenshot in case of failure
+  after(:each) do # Take screenshot in case of failure
     @name_screenshot += @order_of_it.inc.to_s
     if example.exception != nil
       take_screenshot_with_name @name_screenshot
     end
   end
 
-  context "Check strings of 'Builds' tab" do 
+  context "Check strings of 'Builds' tab" do
 
     before(:each) do
       builds(:tab).click; sleep 5
     end
 
-    it "IT_001: brief info checking" do 
-      builds(:app_id_label).text.should eql   $data[:str][$lang][:builds_app_id_label]
-      builds(:version_label).text.should eql  $data[:str][$lang][:builds_version_label]
+    it "IT_001: brief info checking" do
+      builds(:app_id_label).text.should eql $data[:str][$lang][:builds_app_id_label]
+      builds(:version_label).text.should eql $data[:str][$lang][:builds_version_label]
       builds(:phonegap_label).text.should eql $data[:str][$lang][:builds_phonegap_label]
-      builds(:owner_label).text.should eql    $data[:str][$lang][:builds_owner_label]
-      builds(:last_built_label).text.should   include($data[:str][$lang][:builds_last_built_label])
-      builds(:source_label).text.should eql   $data[:str][$lang][:builds_source_label]
+      builds(:owner_label).text.should eql $data[:str][$lang][:builds_owner_label]
+      builds(:last_built_label).text.should include($data[:str][$lang][:builds_last_built_label])
+      builds(:source_label).text.should eql $data[:str][$lang][:builds_source_label]
+      puts "+ <result> IT_001:PASS"
     end
 
     it "IT_002: should match to localized strings: >> iOS <<" do
-      begin
-        timeout(120) {
-          while $data[:str][$lang][:builds_action_pending] == builds(:ios_action).text do
-            @driver.navigate.refresh
-            sleep 5
-            puts "+ action: " + builds(:ios_action).text
-          end
-          break
-        }
-        text=builds(:ios_action).text
-        puts "+ iOS action: " + text
-        if (text ==$data[:str][$lang][:builds_action_error])
-          builds(:ios_action).click
-          if is_element_present(:app_builds_page,:ios_msg)
-            builds(:ios_msg).text.should eql $data[:str][$lang][:error_msg_ios_build_without_signing_key]
-          else
-            puts "+<error> the error message should be display."
-          end
-          builds(:ios_action).click
+
+      timeout(120) {
+        while $data[:str][$lang][:builds_action_pending] == builds(:ios_action).text do
+          @driver.navigate.refresh
+          sleep 5
+          puts "+ action: " + builds(:ios_action).text
         end
-        builds(:ios_rebuild).text.should  eql $data[:str][$lang][:builds_rebuild]
-        builds(:ios_label).text.should    eql $data[:str][$lang][:builds_label]
-        builds(:ios_options_firefox).attribute('label').should  eql $data[:str][$lang][:builds_options]
-        builds(:ios_no_key).text.should   eql $data[:str][$lang][:builds_no_key]
-        builds(:ios_new_key).text.should  eql $data[:str][$lang][:builds_new_key]
-        puts "+ <result> IT_002:PASS"
-      rescue Exception => ex
-        puts "Exception: "
-        puts ex.backtrace
-        puts caller[0]
-        1.should_not eql 1
+        break
+      }
+      text=builds(:ios_action).text
+      puts "+ iOS action: " + text
+      if (text ==$data[:str][$lang][:builds_action_error])
+        builds(:ios_action).click
+        if is_element_present(:app_builds_page, :ios_msg)
+          builds(:ios_msg).text.should eql $data[:str][$lang][:error_msg_ios_build_without_signing_key]
+        else
+          puts "+<error> the error message should be display."
+        end
+        builds(:ios_action).click
       end
+      builds(:ios_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
+      builds(:ios_label).text.should eql $data[:str][$lang][:builds_label]
+      builds(:ios_options_firefox).attribute('label').should eql $data[:str][$lang][:builds_options]
+      builds(:ios_no_key).text.should eql $data[:str][$lang][:builds_no_key]
+      builds(:ios_new_key).text.should eql $data[:str][$lang][:builds_new_key]
+      puts "+ <result> IT_002:PASS"
+
     end
 
-    it "IT_003: should match to localized strings: >> Android <<" do 
+    it "IT_003: should match to localized strings: >> Android <<" do
 
-      begin
-        timeout(120) {
-          while $data[:str][$lang][:builds_action_pending] == builds(:android_action).text do 
-            @driver.navigate.refresh
-            sleep 5
-            puts "+ action: " + builds(:android_action).text
-          end
-          break
-        } 
-        builds(:android_rebuild).text.should  eql $data[:str][$lang][:builds_rebuild]
-        builds(:android_label).text.should    eql $data[:str][$lang][:builds_label]
-        builds(:android_options_firefox).attribute('label').should  eql $data[:str][$lang][:builds_options]
-        builds(:android_no_key).text.should   eql $data[:str][$lang][:builds_no_key]
-        builds(:android_new_key).text.should  eql $data[:str][$lang][:builds_new_key]
-        puts "+ <result> IT_003:PASS"
-      rescue Exception => ex
-        puts "Exception: "
-        puts ex.backtrace
-        puts caller[0]
-        1.should_not eql 1
-      end
+      timeout(120) {
+        while $data[:str][$lang][:builds_action_pending] == builds(:android_action).text do
+          @driver.navigate.refresh
+          sleep 5
+          puts "+ action: " + builds(:android_action).text
+        end
+        break
+      }
+      builds(:android_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
+      builds(:android_label).text.should eql $data[:str][$lang][:builds_label]
+      builds(:android_options_firefox).attribute('label').should eql $data[:str][$lang][:builds_options]
+      builds(:android_no_key).text.should eql $data[:str][$lang][:builds_no_key]
+      builds(:android_new_key).text.should eql $data[:str][$lang][:builds_new_key]
+      puts "+ <result> IT_003:PASS"
+
     end
 
-    it "IT_004: should match to localized strings: >> WinPhone <<" do 
+    it "IT_004: should match to localized strings: >> WinPhone <<" do
 
-      begin 
-        timeout(120) {
-          while $data[:str][$lang][:builds_action_pending] == builds(:winphone_action).text do 
-            @driver.navigate.refresh
-            sleep 5
-            puts "+ action: " + builds(:winphone_action).text
-          end
-          break
-        }
-        builds(:winphone_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
-        puts "+ <result> IT_004:PASS"
-      rescue Exception => ex 
-        puts "Exception: "
-        puts ex.backtrace
-        puts caller[0]
-        1.should_not eql 1
-      end
+      timeout(120) {
+        while $data[:str][$lang][:builds_action_pending] == builds(:winphone_action).text do
+          @driver.navigate.refresh
+          sleep 5
+          puts "+ action: " + builds(:winphone_action).text
+        end
+        break
+      }
+      builds(:winphone_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
+      puts "+ <result> IT_004:PASS"
+
     end
 
-    it "IT_005: should match to localized strings: >> BlackBerry <<" do 
+    it "IT_005: should match to localized strings: >> BlackBerry <<" do
 
-      begin 
-        timeout(120) {
-          while $data[:str][$lang][:builds_action_pending] == builds(:blackberry_action).text do 
-            sleep 5
-            puts "+ action: " + builds(:blackberry_action).text
-          end
-          break
-        }
-        builds(:blackberry_rebuild).text.should  eql $data[:str][$lang][:builds_rebuild]
-        builds(:blackberry_label).text.should    eql $data[:str][$lang][:builds_label]
-        builds(:blackberry_options_firefox).attribute('label').should  eql $data[:str][$lang][:builds_options]
-        builds(:blackberry_no_key).text.should   eql $data[:str][$lang][:builds_no_key]
-        builds(:blackberry_new_key).text.should  eql $data[:str][$lang][:builds_new_key]
-        puts "+ <result> IT_005:PASS"
-      rescue Exception => ex
-        puts "Exception: "
-        puts ex.backtrace
-        puts caller[0]
-        1.should_not eql 1
-      end
+      timeout(120) {
+        while $data[:str][$lang][:builds_action_pending] == builds(:blackberry_action).text do
+          @driver.navigate.refresh
+          sleep 5
+          puts "+ action: " + builds(:blackberry_action).text
+        end
+        break
+      }
+      builds(:blackberry_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
+      builds(:blackberry_label).text.should eql $data[:str][$lang][:builds_label]
+      builds(:blackberry_options_firefox).attribute('label').should eql $data[:str][$lang][:builds_options]
+      builds(:blackberry_no_key).text.should eql $data[:str][$lang][:builds_no_key]
+      builds(:blackberry_new_key).text.should eql $data[:str][$lang][:builds_new_key]
+      puts "+ <result> IT_005:PASS"
+
     end
 
-    it "IT_006: should match to localized strings: >> WebOS <<" do 
+    it "IT_006: should match to localized strings: >> WebOS <<" do
 
-      begin
-        timeout(120) {
-          while $data[:str][$lang][:builds_action_pending] == builds(:webos_action).text do 
-            sleep 5
-            puts "+ action: " + builds(:webos_action).text
-          end
-          break
-        } 
-        builds(:webos_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
-        puts "+ <result> IT_006:PASS"
-      rescue Exception => ex
-        puts "Exception: "
-        puts ex.backtrace
-        puts caller[0]
-        1.should_not eql 1
-      end
+      timeout(120) {
+        while $data[:str][$lang][:builds_action_pending] == builds(:webos_action).text do
+          @driver.navigate.refresh
+          sleep 5
+          puts "+ action: " + builds(:webos_action).text
+        end
+        break
+      }
+      builds(:webos_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
+      puts "+ <result> IT_006:PASS"
+
     end
 
-    it "IT_007: should match to localized strings: >> Symbian <<" do 
+    it "IT_007: should match to localized strings: >> Symbian <<" do
 
-      begin 
-        timeout(120) {
-          while $data[:str][$lang][:builds_action_pending] == builds(:symbian_action).text do 
-            sleep 5
-            puts "+ action: " + builds(:symbian_action).text
-          end  
-          break
-        }
-        builds(:symbian_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
-        puts "+ <result> IT_007:PASS"
-      rescue Exception => ex
-        puts "Exception: "
-        puts ex.backtrace
-        puts caller[0]
-        1.should_not eql 1
-      end
+      timeout(120) {
+        while $data[:str][$lang][:builds_action_pending] == builds(:symbian_action).text do
+          @driver.navigate.refresh
+          sleep 5
+          puts "+ action: " + builds(:symbian_action).text
+        end
+        break
+      }
+      builds(:symbian_rebuild).text.should eql $data[:str][$lang][:builds_rebuild]
+      puts "+ <result> IT_007:PASS"
+
     end
+
   end
 
-  context "Apps without signing-key downloading" do 
+  context "Apps without signing-key downloading" do
 
-    it "IT_008: the >>winphone<< app should be downloaded successfully" do 
+    it "IT_008: the >>winphone<< app should be downloaded successfully" do
 
-      builds(:winphone_action).click; sleep 10
-
-      win = RAutomation::Window.new(:title => /Opening/i)
-      if win.exist? 
-        win.activate; sleep 2; win.send_keys :tab ; sleep 2; win.send_keys :tab ; win.send_keys :enter
-      else 
-        puts "Can not catch the dialog!!!"
+      if $data[:str][$lang][:builds_action_pending] != builds(:winphone_action).text
+        builds(:winphone_action).click; sleep 10
+        download_with_different_browser
+        sleep 10
+        Dir["#{@download_dir}/*.xap"].count.should > 0
+        system("rm #{@download_dir}/*.xap")
+      else
+        puts "+ <result> winphone app was not available"
       end
-      sleep 10
-      Dir["#{@download_dir}/*.xap"].count.should > 0
-      system("rm #{@download_dir}/*.xap")
+
     end
 
-    it "IT_009: the >>webos<< app should be downloaded successfully" do 
-
-      builds(:webos_action).click; sleep 10
-
-      win = RAutomation::Window.new(:title => /Opening/i)
-      if win.exist? 
-        win.activate; sleep 2; win.send_keys :tab ; sleep 2; win.send_keys :tab ; win.send_keys :enter
-      else 
-        puts "Can not catch the dialog!!!"
+    it "IT_009: the >>webos<< app should be downloaded successfully" do
+      if $data[:str][$lang][:builds_action_pending] != builds(:winphone_action).text
+        builds(:webos_action).click; sleep 10
+        download_with_different_browser
+        sleep 10
+        Dir["#{@download_dir}/*.ipk*"].count.should > 0
+        system("rm #{@download_dir}/*.ipk*")
+      else
+        puts "+ <result> webos app was not available"
       end
-      sleep 20
-      Dir["#{@download_dir}/*.ipk"].count.should > 0
-      system("rm #{@download_dir}/*.ipk")
+
     end
 
-    it "IT_010: the >>symbian<< app should be downloaded successfully" do 
+    it "IT_010: the >>symbian<< app should be downloaded successfully" do
 
-      builds(:symbian_action).click; sleep 10
-
-      win = RAutomation::Window.new(:title => /Opening/i)
-      if win.exist? 
-        win.activate; sleep 2; win.send_keys :tab ; sleep 2; win.send_keys :tab ; win.send_keys :enter
-      else 
-        puts "Can not catch the dialog!!!"
+      if $data[:str][$lang][:builds_action_pending] != builds(:winphone_action).text
+        builds(:symbian_action).click; sleep 10
+        download_with_different_browser
+        sleep 10
+        Dir["#{@download_dir}/*.wgz"].count.should > 0
+        system("rm #{@download_dir}/*.wgz")
+      else
+        puts "+ <result> symbian app was not available"
       end
-      sleep 10
-      Dir["#{@download_dir}/*.wgz"].count.should > 0
-      system("rm #{@download_dir}/*.wgz")
+
     end
+
   end
 
 end
