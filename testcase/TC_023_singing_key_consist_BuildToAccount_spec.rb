4a828ac1787c4bac952ef23e3db74d7900af038b
Hanweiwei123 <weiwei.han@dilatoit.com>
2013-10-21 18:09:25 +0800

    update describe "TC_023: "

--- testcase/TC_023_singing_key_consist_BuildToAccount_spec.rb
+++ testcase/TC_023_singing_key_consist_BuildToAccount_spec.rb
@@ -13,7 +13,7 @@ require_relative "../util/webdriver_helper"
 require_relative "../util/dialog_locator/new_app_locator"
 require_relative "../util/dialog_locator/app_id_locator"
 
-describe "TC_015: App Details #Builds" do
+describe "TC_023: singing_key_consist_BuildToAccount" do
   include ConfigParam
   include WebdriverHelper
   include NewAppLocator
