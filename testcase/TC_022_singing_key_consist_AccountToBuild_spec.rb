5a11803e8fb7bf84ea70e0cfc702f6e9586b4ffd
Hanweiwei123 <weiwei.han@dilatoit.com>
2013-10-21 18:08:54 +0800

    update describe "TC_022: "

--- testcase/TC_022_singing_key_consist_AccountToBuild_spec.rb
+++ testcase/TC_022_singing_key_consist_AccountToBuild_spec.rb
@@ -12,7 +12,7 @@ require_relative "../util/webdriver_helper"
 require_relative "../util/dialog_locator/new_app_locator"
 require_relative "../util/dialog_locator/app_id_locator"
 
-describe "TC_009: signing_key_add_and_unlock_rspec" do
+describe "TC_022: singing_key_consist_AccountToBuild" do
   include ConfigParam
   include WebdriverHelper
   include NewAppLocator
