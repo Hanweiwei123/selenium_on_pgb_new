e0f71e83c67399ca08c829253feaa4942975abae
Hanweiwei123 <weiwei.han@dilatoit.com>
2013-10-24 10:25:43 +0800

    update sign_in_github xpath locator

--- testcase/TC_005_register_free_plan_with_github_id_spec.rb
+++ testcase/TC_005_register_free_plan_with_github_id_spec.rb
@@ -7,14 +7,17 @@ require_relative '../action/register_page'
 require_relative '../util/config_param'
 require_relative '../util/webdriver_helper'
 require_relative '../util/dialog_locator/register_locator'
+require_relative '../util/dialog_locator/sign_in_github_locator'
 require_relative '../util/dialog_locator/header_footer_locator'
 
 # This TC describes:
 #   situations trying to register github free account to sign in. 
 describe "TC_005: Register an free plan account with Github ID" do 
-    include RegisterLocator
+    
     include ConfigParam
     include WebdriverHelper
+    include RegisterLocator
+    include SignInGithubLocator
     include HeaderFooterLocator
 
     before(:all) do 
@@ -29,7 +32,7 @@ describe "TC_005: Register an free plan account with Github ID" do
 	@driver.manage.window.maximize
 	@driver.manage.timeouts.implicit_wait = 30
         @driver.get path_format_locale("/plans/free", @base_url)
-        register_locator(:github_btn).click; sleep 10
+        register_locator(:github_btn).click; sleep 5
     end
 
     after(:each) do  # Take screenshot in case of failure
@@ -44,29 +47,31 @@ describe "TC_005: Register an free plan account with Github ID" do
     end
 
     it "IT_001: direct to '/apps' page when sign in successfully with github id(which is connected to pgb), " do 
-        fill_in register_locator(:github_login_username_input), :with => $data[:user][$lang][:github_id_only][:id]
-        fill_in register_locator(:github_login_password_input), :with => $data[:user][$lang][:github_id_only][:password]
-        register_locator(:github_login_submit_btn).click;  sleep 10
+        fill_in sign_in_github_locator(:username_or_email), :with => $data[:user][$lang][:github_id_only][:id]
+        fill_in sign_in_github_locator(:password), :with => $data[:user][$lang][:github_id_only][:password]
+        sign_in_github_locator(:sign_in_btn).click;  sleep 10
         
         @driver.current_url.should eql @base_url + $data[:url][:sign_in_successfully]
         sign_out
     end
 
     it "IT_002: Got an error message('Existing Registration Found') when use account that can log in both. " do  
-        fill_in register_locator(:github_login_username_input), :with => $data[:user][$lang][:email_that_login_both][:id]
-        fill_in register_locator(:github_login_password_input), :with => $data[:user][$lang][:email_that_login_both][:password]
-        register_locator(:github_login_submit_btn).click; sleep 5
+        fill_in sign_in_github_locator(:username_or_email), :with => $data[:user][$lang][:email_that_login_both][:id]
+        fill_in sign_in_github_locator(:password), :with => $data[:user][$lang][:email_that_login_both][:password]
+        sign_in_github_locator(:sign_in_btn).click; sleep 5
+        
         register_locator(:existing_registration_found_warning).text.should eql $data[:str][$lang][:PGB_existing_registration_found]
     end
 
     it "IT_003: Got an error message when proceed without selecting a country" do 
-        fill_in register_locator(:github_login_username_input), :with => $data[:user][$lang][:adobe_id_free_final_step][:id]
-        fill_in register_locator(:github_login_password_input), :with => $data[:user][$lang][:adobe_id_free_final_step][:password]
-        register_locator(:github_login_submit_btn).click; sleep 5
+        fill_in sign_in_github_locator(:username_or_email), :with => $data[:user][$lang][:adobe_id_free_final_step][:id]
+        fill_in sign_in_github_locator(:password), :with => $data[:user][$lang][:adobe_id_free_final_step][:password]
+        sign_in_github_locator(:sign_in_btn).click; sleep 5
+        
         @current_url = @driver.current_url
         puts "+<current_url> is #{@current_url}"
         if(@current_url.include? "https://github.com/login/oauth/")
-          register_locator(:github_allow_access_btn).click
+          sign_in_github_locator(:allow_access_btn).click
           sleep 3
         end
         register_locator(:github_accept_the_adobe_terms).click
@@ -75,9 +80,10 @@ describe "TC_005: Register an free plan account with Github ID" do
     end
 
     it "IT_004: Got an error message when proceed without agree to the terms of service" do 
-        fill_in register_locator(:github_login_username_input), :with => $data[:user][$lang][:adobe_id_free_final_step][:id]
-        fill_in register_locator(:github_login_password_input), :with => $data[:user][$lang][:adobe_id_free_final_step][:password]
-        register_locator(:github_login_submit_btn).click; sleep 5
+        fill_in sign_in_github_locator(:username_or_email), :with => $data[:user][$lang][:adobe_id_free_final_step][:id]
+        fill_in sign_in_github_locator(:password), :with => $data[:user][$lang][:adobe_id_free_final_step][:password]
+        sign_in_github_locator(:sign_in_btn).click; sleep 5
+        
         register_locator(:github_select_a_country).find_elements(:tag_name => "option").each do |country| 
             if(country.text == "Japan") 
                 country.click
@@ -89,9 +95,10 @@ describe "TC_005: Register an free plan account with Github ID" do
     end
 
     it "IT_005: page direct to '/apps' after sign in successfully with valid appropriate account . " do 
-        fill_in register_locator(:github_login_username_input), :with => $data[:user][$lang][:adobe_id_free_final_step][:id]
-        fill_in register_locator(:github_login_password_input), :with => $data[:user][$lang][:adobe_id_free_final_step][:password]
-        register_locator(:github_login_submit_btn).click; sleep 5
+        fill_in sign_in_github_locator(:username_or_email), :with => $data[:user][$lang][:adobe_id_free_final_step][:id]
+        fill_in sign_in_github_locator(:password), :with => $data[:user][$lang][:adobe_id_free_final_step][:password]
+        sign_in_github_locator(:sign_in_btn).click; sleep 5
+        
         register_locator(:github_select_a_country).find_elements(:tag_name => "option").each do |country| 
             if(country.text == "United States") 
                 country.click
