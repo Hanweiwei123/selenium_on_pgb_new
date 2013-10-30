b3d100de0e760c3644253e429479085929265574
Hanweiwei123 <weiwei.han@dilatoit.com>
2013-10-30 17:15:56 +0800

    update code format

--- testcase/TC_011_plugins_after_sign_in_spec.rb
+++ testcase/TC_011_plugins_after_sign_in_spec.rb
@@ -11,11 +11,6 @@ require_relative '../util/dialog_locator/plugins_locator'
 require_relative '../util/dialog_locator/header_footer_locator'
 
 # This TC describes:
-#   situations:
-#       Sign in with Adobe ID
-#       Sign in with Github ID
-#       Forgot password
-#       Didn't receive confirmation
 describe "TC_011: Plugins" do
   include SignInLocator
   include PluginsLocator
@@ -35,14 +30,15 @@ describe "TC_011: Plugins" do
     @sign_in_page = SignInPage.new(
         :driver => @driver,
         :base_url => @base_url,
-        :user => {:id => $data[:user][$lang][:adobe_id_free_002][:id], :password => $data[:user][$lang][:adobe_id_free_002][:password]} )
+        :user => {:id => $data[:user][$lang][:adobe_id_free_002][:id],
+                  :password => $data[:user][$lang][:adobe_id_free_002][:password]})
     @sign_in_page.sign_in_with_adobe_id
     sleep 5
     @driver.current_url.should == @base_url + $data[:url][:sign_in_successfully]
     header_get(:main_nav_link_plugins).click
   end
 
-  after(:each) do  # Take screenshot in case of failure
+  after(:each) do # Take screenshot in case of failure
     @name_screenshot += @order_of_it.inc.to_s
     if example.exception != nil
       take_screenshot_with_name @name_screenshot
@@ -57,53 +53,55 @@ describe "TC_011: Plugins" do
     plugin_locator(:tab_your_plugins).click
     @tab_your_plugins = plugin_locator(:you_have_no_plugins).text
     @tab_your_plugins.should eql $data[:str][$lang][:plugin_you_have_no_plugins]
+  end
+
+  describe "Trying to submit plugin with Adobe ID" do
+    before(:all) do
+      plugin_locator(:tab_submit_plugin).click
     end
 
-      describe "Trying to submit plugin with Adobe ID" do
-        before(:all) do
-          plugin_locator(:tab_submit_plugin).click
-        end
+    it "IT_002: should match to localized error msg when filling in repo url with invalid one" do
+      plugin_locator(:plugin_git_repository_url).send_keys($data[:plugin][:invalid_plugin][:url])
+      plugin_locator(:optional_tag_or_branch).attribute("placeholder").should eql "master"
+      plugin_locator(:btn_submit_plugin).click
+      plugin_locator(:checkbox_accept_license).click
+      plugin_locator(:btn_submit).click
+      sleep 10
+      @plugin_git_repository_url_msg = plugin_locator(:header_notifications).text
+      @plugin_git_repository_url_msg.should eql $data[:str][$lang][:plugin_uri_invalid_msg]
+    end
 
-        it "IT_002: should match to localized error msg when filling in repo url with invalid one" do
-          plugin_locator(:plugin_git_repository_url).send_keys($data[:plugin][:invalid_plugin][:url])
-          plugin_locator(:optional_tag_or_branch).send_keys($data[:plugin][:new_plugin][:branch])
-          plugin_locator(:btn_submit_plugin).click
-          plugin_locator(:checkbox_accept_license).click
-          plugin_locator(:btn_submit).click
-          @plugin_git_repository_url_msg = plugin_locator(:header_notifications).text
-          @plugin_git_repository_url_msg.should eql $data[:str][$lang][:plugin_uri_invalid_msg]
-        end
-
-        it "IT_003: should submit plugin successfully" do
-          plugin_locator(:plugin_git_repository_url).clear
-          plugin_locator(:plugin_git_repository_url).send_keys($data[:plugin][:new_plugin][:url])
-          plugin_locator(:btn_submit_plugin).click
-          plugin_locator(:checkbox_accept_license).click
-          plugin_locator(:btn_submit).click # will submit the plugin
-          sleep 20
-          @driver.current_url.should =~ /\/plugins\/\d+/
-          @plugin_submit_successfully_msg = plugin_locator(:header_notifications).text
-          @plugin_submit_successfully_msg.should eql $data[:str][$lang][:plugin_submit_successfully_msg]
-        end
+    it "IT_003: should submit plugin successfully" do
+      plugin_locator(:plugin_git_repository_url).clear
+      plugin_locator(:plugin_git_repository_url).send_keys($data[:plugin][:new_plugin][:url])
+      plugin_locator(:btn_submit_plugin).click
+      plugin_locator(:checkbox_accept_license).click
+      plugin_locator(:btn_submit).click # will submit the plugin
+      sleep 20
+      @driver.current_url.should =~ /\/plugins\/\d+/
+      @plugin_submit_successfully_msg = plugin_locator(:header_notifications).text
+      @plugin_submit_successfully_msg.should eql $data[:str][$lang][:plugin_submit_successfully_msg]
+    end
 
 
-        it "IT_004: should match to localized msg 'duplicated'" do
-        #  @driver.get path_format_locale("/plugins","")
-          # @driver.get "@base_url.to_s + /plugins"
-          header_get(:main_nav_link_plugins).click
-          plugin_locator(:tab_submit_plugin).click
-          plugin_locator(:plugin_git_repository_url).clear
-          plugin_locator(:plugin_git_repository_url).send_keys($data[:plugin][:new_plugin][:url])
-          plugin_locator(:optional_tag_or_branch).send_keys($data[:plugin][:new_plugin][:branch])
-          plugin_locator(:btn_submit_plugin).click
-          plugin_locator(:checkbox_accept_license).click
-          plugin_locator(:btn_submit).click # will submit the plugin
-          sleep 10
-          @driver.current_url.should eql @base_url+"\/plugins#add"
-          @plugin_submit_duplicated_msg = plugin_locator(:header_notifications).text
-          @plugin_submit_duplicated_msg.should eql $data[:str][$lang][:plugin_submit_duplicated_msg]
-        end
+    it "IT_004: should match to localized msg 'duplicated'" do
+      #  @driver.get path_format_locale("/plugins","")
+      # @driver.get "@base_url.to_s + /plugins"
+      header_get(:main_nav_link_plugins).click
+      plugin_locator(:tab_submit_plugin).click
+      plugin_locator(:plugin_git_repository_url).clear
+      plugin_locator(:plugin_git_repository_url).send_keys($data[:plugin][:new_plugin][:url])
+      plugin_locator(:optional_tag_or_branch).send_keys($data[:plugin][:new_plugin][:branch])
+      plugin_locator(:btn_submit_plugin).click
+      plugin_locator(:checkbox_accept_license).click
+      plugin_locator(:btn_submit).click # will submit the plugin
+      sleep 10
+      @driver.current_url.should eql @base_url+"\/plugins#add"
+      @plugin_submit_duplicated_msg = plugin_locator(:header_notifications).text
+      @plugin_submit_duplicated_msg.should eql $data[:str][$lang][:plugin_submit_duplicated_msg]
+    end
 
-      end
+  end
+
 end
 
